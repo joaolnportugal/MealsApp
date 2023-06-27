92059628b923caf6813eb10ce718e7ca59fa5d46
TUGZ_PC\joaol <joaolnportugal95@gmail.com>
2023-06-27 02:27:47 +0100

    main grid with colors

---
+++ screens/CategoryScreen.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { View, Text, FlatList } from "react-native";
+import { CATEGORIES } from "../data/dummy-data";
+import CategoryGridTile from "../components/CategoryGridTile";
+
+function renderCategoryItem(itemData) {
+  return (
+    <CategoryGridTile title={itemData.item.title} color={itemData.item.color} />
+  );
+}
+
+function CategoryScreen() {
+  return (
+    <FlatList
+      data={CATEGORIES}
+      keyExtractor={item => item.id}
+      renderItem={renderCategoryItem}
+      numColumns={2}
+    />
+  );
+}
+export default CategoryScreen;
